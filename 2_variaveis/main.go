601f54d931cf1dc70c6f4599c01473737f57d0fd
Walisson Santos <walissonsantost008@gmail.com>
2025-03-04 11:58:15 -0300

    Aprendendo como criar variaveis e constantes no go

---
+++ 2_variaveis/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "walisson"
+	fmt.Println(nome)
+
+	var (
+		variavel1 string = "Vai te fuder"
+		variavel2 string = "Pega na minha pomba"
+	)
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+	const idade int = 17
+}

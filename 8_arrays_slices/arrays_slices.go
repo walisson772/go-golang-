e4d00796dcfb669bf37b9225432aaf39792c61a4
Walisson Santos <walissonsantost008@gmail.com>
2025-03-05 19:23:27 -0300

    Apendendo sobre Arrays Internos

--- 8_arrays_slices/arrays_slices.go
+++ 8_arrays_slices/arrays_slices.go
@@ -29,4 +29,23 @@ func main() {
 	array2[1] = "Posição alterada"
 	fmt.Println(slice2)
 
+	// Arrays Internos
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	slice4 = append(slice4, 10)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
 }
